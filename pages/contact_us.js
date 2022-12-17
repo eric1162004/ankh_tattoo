82c3d932445dc77e0dc5e45c5fbeb9b0eb328f78
Eric Cheung <eric1162004@hotmail.com>
2022-12-17 00:28:38 -0800

    update

--- pages/contact_us.js
+++ pages/contact_us.js
@@ -6,11 +6,11 @@ export default function Contact_us() {
   return (
     <div className="max-h-screen overflow-hidden">
       {/* Contact and Map */}
-      <div className="grid grid-cols-1 md:grid-cols-2 p-5 w-full h-full md:gap-28 top-0 z-10 absolute overflow-scroll text-sm md:text-base">
-        <div className="flex flex-col h-full items-end justify-center grayscale overflow-hidden mt-10 md:mt-0">
+      <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center p-5 w-full h-full md:gap-28 top-10 md:top-0 z-10 absolute overflow-scroll text-sm md:text-base">
+        <div className="flex flex-col md:items-end md:justify-center grayscale overflow-hidden  md:mt-0">
           {<Map lat={49.26666} lng={-123.11149} zoom={15} />}
         </div>
-        <div className="flex flex-col h-full items-start justify-center  text-primary">
+        <div className="flex flex-col h-full md:items-start md:justify-center  text-primary">
           <div className="text-center backdrop-brightness-50 p-5 md:p-16">
             <h1 className="title text-2xl my-0 tracking-wider">Contact us</h1>
             <p>Studio address</p>
