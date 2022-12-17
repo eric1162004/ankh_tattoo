cf7b4cfca4e40fdadd62dd673d490046896fa3ee
Eric Cheung <eric1162004@hotmail.com>
2022-12-16 23:05:43 -0800

    update

--- pages/faq.js
+++ pages/faq.js
@@ -5,30 +5,28 @@ import VideoBackgroundDiv from "../components/VideoBackgroundDiv";
 
 export default function Faq() {
   return (
-    <div className="md:text-lg h-full w-full">
+    <div className="text-base md:text-lg h-full w-full">
       {/* FAQ video background */}
       <VideoBackgroundDiv
-        className="h-full w-full backdrop-blur-xs "
+        className="h-full w-full"
         height={100}
         video="video/faq.mov"
       >
         <div className="backdrop-brightness-50 md:px-32 text-primary ">
-          <div className="flex pt-36 justify-center items-center p-5 ">
-            <div className="">
-              <h1 className="title">Frequently Asked Question</h1>
-              <p className="paragraph">
-                Tattoo is permanent and it’s a big decision and commitment in
-                life. You may have a lot of question and concern before making
-                the decision. Please read the FAQ section below to find the
-                answer of most frequently asked question, and feel free to
-                contact me about your concern.
-              </p>
-            </div>
+          <div className="flex flex-col pt-28 justify-center items-center p-5 ">
+            <h1 className="title">Frequently Asked Question</h1>
+            <p className="paragraph">
+              Tattoo is permanent and it’s a big decision and commitment in
+              life. You may have a lot of question and concern before making the
+              decision. Please read the FAQ section below to find the answer of
+              most frequently asked question, and feel free to contact me about
+              your concern.
+            </p>
           </div>
           <div className="">
             {data["faq"].map((item, index) => (
               <div
-                className="collapse collapse-arrow backdrop-opacity-50 backdrop-blur-lg m-2"
+                className="collapse collapse-arrow backdrop-opacity-50 backdrop-blur-lg mx-2"
                 key={index}
               >
                 <input type="checkbox" />
