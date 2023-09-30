11004c2c61086126b782d0c05484d079441b3188
Eric Cheung <eric1162004@hotmail.com>
2023-09-30 00:14:56 -0700

    update

--- pages/tattoo_revamp.js
+++ pages/tattoo_revamp.js
@@ -107,7 +107,7 @@ export default function Tattoo_revamp({ data }) {
                       <img
                         className="block object-cover object-center w-full h-full rounded-lg hover:brightness-90 hover:contrast-125 "
                         src={image.src}
-                        alt={`Vancouver Fineline Tattoo - ${collections[collectionIndex]} image`}
+                        alt={`Vancouver Fineline Tattoos - ${collections[collectionIndex]} image`}
                       />
                     </LazyLoad>
                   </div>
