6f5a6ad92b452b544d149af07eaacc489ad00cd7
Eric Cheung <eric1162004@hotmail.com>
2024-04-08 22:25:45 -0700

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import GoogleTagManager from "../hooks/GoogleTagManager";
 
 export default function Document() {
   return (
@@ -17,7 +16,6 @@ export default function Document() {
           name="description"
           content="Fineline tattoo is subtle, delicate, and thin-line tattoo. Unlike traditional tattoos, which create thicker and bolder design, fine-line tattoos are created by just a single tattoo needle or combinations of small needles. This style of tattoo allows artist to create soft, tiny and minimalistic tattoos with intricately detailed design by using ultra-thin line incorporating with color, shading, and texture. It’s a growing trend in past decade to make delicate hidden tattoos, with super skinny lines and minimalist designs."
         ></meta>
-        <GoogleTagManager />
       </Head>
       <body>
         <Main />
